0d8d5f97e52bf5d777bb7ab1728be96eaba473a0
Martinsr99 <silesreche.martin@gmail.com>
2023-03-18 15:56:54 +0100

    Feature: Finish routes and add validators/middlewares

---
+++ controllers/auth.js
@@ -0,0 +1,39 @@
+const {validationResult} = require('express-validator')
+
+
+const crearUsuario = (req, res) => {
+
+  const {name,email,password} = req.body
+
+
+
+  res.status(201).json({
+    ok: true,
+    msg:'registro',
+    name,
+    email,
+    password
+  });
+};
+
+const loginUsuarios = (req, res) => {
+
+  const {email,password} = req.body
+
+
+  res.status(200).json({
+    ok: true,
+    msg: "login",
+    email,
+    password
+  });
+};
+
+const revalidarToken = (req, res) => {
+  res.json({
+    ok: true,
+    msg: "renew",
+  });
+};
+
+module.exports = { crearUsuario,loginUsuarios,revalidarToken };
--- routes/auth.js
+++ routes/auth.js
@@ -3,14 +3,43 @@
     host + /api/auth
 */
 
-const {Router} = require('express');
-const { crearUsuario, loginUsuarios, revalidarToken } = require('./controllers/auth');
-const router = Router()
+const { Router } = require("express");
+const { check } = require("express-validator");
+const {
+  crearUsuario,
+  loginUsuarios,
+  revalidarToken,
+} = require("../controllers/auth");
+const { validarCampos } = require("../middlewares/validar-campos");
+const router = Router();
 
-router.post("/new", crearUsuario);
+router.post(
+  "/new",
+  [
+    check("name", "El nombre es obligatorio").not().isEmpty(),
+    check("email", "El email es obligatorio").isEmail(),
+    check(
+      "password",
+      "La contraseña debe tener al menos 6 caracteres"
+    ).isLength({ min: 6 }),
+    validarCampos
+  ],
+  crearUsuario
+);
 
-router.post("/", loginUsuarios );
+router.post(
+  "/",
+  [
+    check("email", "El email es obligatorio").isEmail(),
+    check(
+      "password",
+      "La contraseña debe tener al menos 6 caracteres"
+    ).isLength({ min: 6 }),
+    validarCampos
+  ],
+  loginUsuarios
+);
 
-router.get("/renew",revalidarToken );
+router.get("/renew", revalidarToken);
 
 module.exports = router;
