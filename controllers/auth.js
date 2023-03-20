af8c2fe33a3aea366305b26a53eb9ba815b8e668
Martinsr99 <silesreche.martin@gmail.com>
2023-03-20 21:29:17 +0100

    Create login and add JWT

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,7 @@
 const {validationResult} = require('express-validator')
 const Usuario = require('../models/Usuario')
 const bcrypt = require('bcryptjs')
+const {generarJWT} = require('../helpers/jwt')
 
 const crearUsuario = async (req, res) => {
 
@@ -23,12 +24,16 @@ const crearUsuario = async (req, res) => {
     const salt = bcrypt.genSaltSync()
     usuario.password = bcrypt.hashSync(password,salt)
 
+    //Generar JWT
+    const token = await generarJWT(usuario.id,usuario.name)
+
     await usuario.save()
   
     res.status(201).json({
       ok: true,
       uid: usuario.id,
-      name: usuario.name
+      name: usuario.name,
+      token
     });
 
   } catch (error) {
@@ -40,17 +45,47 @@ const crearUsuario = async (req, res) => {
 
 };
 
-const loginUsuarios = (req, res) => {
+const loginUsuarios = async (req, res) => {
 
   const {email,password} = req.body
 
+  try {
+    const usuario = await Usuario.findOne({email})
+    
+    if (!usuario) {
+      return res.status(400).json({
+        ok:false,
+        msg: 'El usuario no existe con ese email'
+      })
+    }
+    
+    const validPassword = bcrypt.compareSync(password, usuario.password)
+    
+    if(!validPassword) {
+      return res.status(400).json({
+        ok:false,
+        msg: 'La contraseña no es correcta'
+      })
+    }
 
-  res.status(200).json({
-    ok: true,
-    msg: "login",
-    email,
-    password
-  });
+    //Generar JWT
+    const token = await generarJWT(usuario.id,usuario.name)
+
+
+    res.status(200).json({
+      ok:true,
+      uid:usuario.id,
+      name: usuario.name,
+      token
+    })
+
+  } catch (error) {
+    res.status(500).json({
+      ok: false,
+      msg: 'No se pudo loggear el usuario'
+    })
+  }
+
 };
 
 const revalidarToken = (req, res) => {
